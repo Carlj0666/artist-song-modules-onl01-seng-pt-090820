487938d8617edb310335fe43f1aa3f02775dd9c2
Carl J <carlj1881@yahoo.com>
2020-10-08 23:54:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ require 'pry'
 class Artist
   
   extend Memorable::ClassMethods
-  include Memorable::ClassMethods, Findable, 
+  include Memorable::ClassMethods, Findable, Paramable
   
   attr_accessor :name
   attr_reader :songs
